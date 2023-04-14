7685bc26ea4fb21c2ecf5a358dfad37ec0d6a42c
Murilo <murilo.ramos@rspneus.com.br>
2023-04-14 17:44:24 -0300

    deu boa

--- src/components/weeklyHighlights.tsx
+++ src/components/weeklyHighlights.tsx
@@ -1,45 +1,38 @@
-import { Button, Flex, Img, Text } from "@chakra-ui/react";
-import { api } from "../services/api";
-import { useState } from "react";
+// pages/index.js
+
+import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y } from "swiper";
+
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper";
+const images = [
+  "https://placehold.co/480x300?font=roboto&text=Slide+1",
+  "https://placehold.co/480x300?font=roboto&text=Slide+2",
+  "https://placehold.co/480x300?font=roboto&text=Slide+3",
+  "https://placehold.co/480x300?font=roboto&text=Slide+4",
+];
 
-export function WeeklyHighLights() {
+export default function WeeklyHighLights() {
   return (
-    <>
-      <Flex w="100%" h="100%" justify="center">
-        <Flex w="100%" px="15rem">
-          <Swiper
-            loop={true}
-            slidesPerView={3}
-            spaceBetween={30}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Pagination, Navigation]}
-            className="mySwiper"
-          >
-            {" "}
-            <SwiperSlide>
-              <Img src="/Image/brutus.png" w="full" />
-            </SwiperSlide>{" "}
-            <SwiperSlide>
-              <Img src="/Image/brutus.png" w="full" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Img src="/Image/brutus.png" w="full" />
-            </SwiperSlide>
-            <SwiperSlide />
-            <SwiperSlide />
-            <SwiperSlide />
-          </Swiper>
-        </Flex>
-      </Flex>
-      {/* Outros componentes de UI */}
-    </>
+    <Flex w="full" align="center" h="100rem">
+      <Swiper
+        modules={[Navigation, Pagination, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation={true}
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log("slide change")}
+        // onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </Flex>
   );
 }
